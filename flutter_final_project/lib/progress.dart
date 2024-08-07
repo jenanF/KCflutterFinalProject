4e1a64fb64e67291adc29304a98d60a03e7b74b2
jenanF <108689302+jenanF@users.noreply.github.com>
2024-08-08 01:20:41 +0300

    d

--- flutter_final_project/lib/progress.dart
+++ flutter_final_project/lib/progress.dart
@@ -5,8 +5,129 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(child: Text("category")),
+    double category_height = 180;
+    double category_width = 170;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 65,
+        flexibleSpace: Center(
+          child: Image.asset(
+            'assets/images/white.png',
+            width: 70,
+            height: 70,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Spacer(),
+            Text("Improvments",
+                style: TextStyle(
+                    color: Colors.pink,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800)),
+            Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: category_height,
+                  width: category_width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color.fromARGB(255, 230, 248, 211)),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/healthred.png"),
+                      Text(
+                        "Health",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  height: category_height,
+                  width: category_width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color.fromARGB(255, 255, 254, 189)),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/mental.png"),
+                      Text("Mental",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: category_height,
+                  width: category_width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color.fromARGB(255, 238, 221, 245)),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/work.png"),
+                      Text(
+                        "Work",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  height: category_height,
+                  width: category_width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color.fromARGB(255, 201, 236, 254)),
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/creativity.png"),
+                      Text("Get Inspired",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Spacer(),
+          ],
+        ),
+      )),
     );
   }
 }
